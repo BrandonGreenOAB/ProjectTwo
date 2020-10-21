adae81f1303eb12cc1e787ce290bfbba61e60c86
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-10-21 16:58:11 -0400

    config.js changes

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS dock_db;
+CREATE DATABASE dock_db;
+USE DATABASE dock_db;
+
+CREATE TABLE languages (
+    languageid int not null AUTO_INCREMENT,
+    langName VARCHAR(255),
+    PRIMARY KEY (languageid)
+);
+
+CREATE TABLE jobs (
+    jobid int not null AUTO_INCREMENT,
+    jobName VARCHAR(255),
+    price decimal(15,2),
+    jobDone BOOLEAN NOT NULL default 0
+    PRIMARY key (jobid)
+);
+
+CREATE TABLE userOption (
+    jobsOpen INT(255),
+    jobsDone INT(255),
+    languages VARCHAR(255)
+)
